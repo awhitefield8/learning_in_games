07e345c4cdf5bf93356dee5e7df23fe5a1382005
Alexander Whitefield <59574428+awhitefield8@users.noreply.github.com>
2023-12-15 13:34:06 -0500

    Add files via upload

--- funcs.py
+++ funcs.py
@@ -22,55 +22,42 @@ def inverse_mw(new_weight,old_weight,nu=0.1):
 
 ### value iteration functions
 
-def value_it_fixedComp(s_grid,
-                       p_grid,
-                       outsidePolicy = 0.5,
-                       nu = 0.2,
-                       beta = 0.95,
-                       max_iterations = 200):
+def value_it_fixedComp(s_grid_,
+                       p_grid_,
+                       outsidePolicy=0.5,
+                       nu=0.2,
+                       beta=0.95,
+                       max_iterations=2000):
+
     """
     value iteration when competitors always charge a fixed price
     Args,
-        s_grid: grid of states
-        p_prid: grid of policies
+        s_grid (list): grid of states
+        p_grid (list): grid of policies
     Returns:
         Dictionary of value and policy functions: {'value_function','policy_function'}
     """
 
-    v_curr = [1]*len(s_grid)
-    p_curr = [1]*len(s_grid) #policy function ('grid')
 
-
-
-    ### idea: start with a grid of states. generate
-    # i) an interpolated value function (for the player to estimte future values)
-    # ii) an interpolated policy function (to simulate the other players play)
-
+    s_grid = np.array(s_grid_)
+    p_grid = np.array(p_grid_)
+    v_curr = np.ones(len(s_grid_))
+    p_curr = np.ones(len(s_grid_), dtype=int)  # Initialize as integers for indexing
 
     for it in range(max_iterations):
-        #loop through states, and for each, pick an optimal policy, and update corresponding value
-        v_new = [-1]*len(v_curr) #prep new list
-        # define functions for analysis
-        interp_v_func = interp1d(s_grid, #grid points
-                                v_curr, #values
-                                kind='linear', fill_value='extrapolate')
-        for j in range(len(s_grid)):
-            s = s_grid[j]
-            values = [-1]*len(p_grid)
-            for i in range(len(p_grid)):
-                p = p_grid[i]
-                flow_u = p*s
-                new_weight_a = s*(1 - (nu*p))
-                new_weight_b = (1-s)*(1 - (nu*outsidePolicy))
-                new_state = new_weight_a / (new_weight_a + new_weight_b)
-                future_u = beta*interp_v_func(new_state)
-                ### interpolating function
-                values[i]= flow_u + future_u
-            v_new[j] = np.array(values).max()
-            p_curr[j] = p_grid[np.array(values).argmax()]    ### break ties by picking lowest price. policy correspondence is not convex. >>> could extend by breaking randomly
+        interp_v_func = interp1d(s_grid, v_curr, kind='linear', fill_value='extrapolate')
 
-        if np.linalg.norm(np.array(v_new) - np.array(v_curr)) < 0.001:
-            print('coverged successfully in ' + str(it) + ' iterations')
+        new_weight_a = s_grid[:, np.newaxis] * (1 - nu * p_grid)
+        new_weight_b = (1 - s_grid[:, np.newaxis]) * (1 - nu * outsidePolicy)
+        new_state = new_weight_a / (new_weight_a + new_weight_b)
+        future_u = beta * interp_v_func(new_state)
+
+        values = p_grid[np.newaxis, :] * s_grid[:, np.newaxis] + future_u
+        v_new = np.max(values, axis=1)
+        p_curr = p_grid[np.argmax(values, axis=1)]
+
+        if np.linalg.norm(v_new - v_curr) < 0.001:
+            print('converged successfully in ' + str(it) + ' iterations')
             break
         else:
             v_curr = v_new
@@ -78,8 +65,58 @@ def value_it_fixedComp(s_grid,
         if it == (max_iterations - 1):
             print('did not terminate after ' + str(it) + ' iterations')
 
-    return({'value_function': v_curr,'policy_function': p_curr})
+    return {'value_function': v_curr.tolist(), 'policy_function': p_curr.tolist()}
 
+
+
+
+def value_it_stratComp(s_grid,
+                        p_grid,
+                        nu=0.2,
+                        beta=0.95,
+                        max_iterations=2000):
+    """
+    Vectorized version of value_it_stratComp2
+    Args:
+        s_grid: grid of states
+        p_grid: grid of policies
+        nu: nu parameter
+        beta: beta parameter
+        max_iterations: maximum number of iterations
+    Returns:
+        Dictionary of value and policy functions: {'value_function', 'policy_function'}
+    """
+    s_grid = np.array(s_grid)
+    p_grid = np.array(p_grid)
+
+    v_curr = np.ones(len(s_grid))
+    p_curr = np.ones(len(s_grid), dtype=int)  # Initialize as integers for indexing
+    
+    for it in range(max_iterations):
+        interp_v_func = interp1d(s_grid, v_curr, kind='linear', fill_value='extrapolate')
+        p_curr_b = p_curr[::-1]
+
+        new_weight_a = s_grid[:, np.newaxis] * (1 - (nu * p_grid))
+        new_weight_b = np.tile(np.multiply((1 - (nu * p_curr_b)),(1 - s_grid[:, np.newaxis]).T).T,len(p_grid))
+        #new_weight_b = new_weight_a[::-1]
+
+        new_state = new_weight_a / (new_weight_a + new_weight_b)
+        future_u = beta * interp_v_func(new_state)
+
+        values = p_grid[np.newaxis, :] * s_grid[:, np.newaxis] + future_u
+        v_new = np.max(values, axis=1)
+        p_curr = p_grid[np.argmax(values, axis=1)]
+
+        if np.linalg.norm(v_new - v_curr) < 0.001:
+            print('converged successfully in ' + str(it) + ' iterations')
+            break
+        else:
+            v_curr = v_new
+
+        if it == (max_iterations - 1):
+            print('did not terminate after ' + str(it) + ' iterations')
+
+    return {'value_function': v_curr.tolist(), 'policy_function': p_curr.tolist()}
 
 
 
